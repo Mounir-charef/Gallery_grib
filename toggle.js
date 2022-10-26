a845c77957ea2d88f8051ef8f8af8ef674a45beb
Mounir <polomnr1@gmail.com>
2022-10-26 15:02:37 +0100

    Finished

--- toggle.js
+++ toggle.js
@@ -14,11 +14,20 @@ window.matchMedia("(max-width: 800px)").onchange = () => {
     nav_bar.dataset.toggled = "false";
 };
 // cursor_wrapper
-
+function isTouchDevice(){
+    try{
+        document.createEvent("TouchEvent");
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
 
 let myDiv = document.getElementById('follower');
 
-let x,y;
+myDiv.style.display = isTouchDevice() ? "none" : "block";
+console.log(isTouchDevice())
+
 const move = e => {
     myDiv.style.left = e.pageX  + "px";
     myDiv.style.top = e.pageY + "px";
